f344412be1b7855a84bc83d461caa994e1d1ade0
jose cartin <jose.cartin.cr@hotmail.com>
2021-02-20 22:11:55 -0600

    Se agrega el menu al sitio.

---
+++ js/base.js
@@ -0,0 +1,9 @@
+function AbrirMenu() {
+    document.getElementById('MenuPrincipal').style.width = "250px"
+    document.getElementById('Menu').style.overflow = 'hidden'
+}
+
+function CerrarMenu() {
+    document.getElementById('MenuPrincipal').style.width = "0"
+    document.getElementById('Menu').style.overflow = 'display'
+}
